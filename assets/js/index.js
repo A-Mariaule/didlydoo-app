4bee25c771401856ade4180d87727028a390e5e4
Aurélien Mariaule <mariaule.aurelien@gmail.com>
2023-01-05 16:46:33 +0100

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ fetch("http://localhost:3000/api/events/")
       title.textContent = elem.name;
       //organisateur
       let organizer = document.createElement("p");
-      organizer.className = "card_organizer";
+      organizer.className = "card__organizer";
       card.appendChild(organizer);
       organizer.textContent = elem.author;
       //description
@@ -25,12 +25,41 @@ fetch("http://localhost:3000/api/events/")
       description.className = "card__description";
       card.appendChild(description);
       description.textContent = elem.description;
-      //liste invité
-      const fetchName = (name) => fetch("http://localhost:3000/api/attendees/");
-      fetchName(elem.name)
-        .then((response) => response.json())
-        .then((attendees) => {
-          console.log(attendees);
-        });
+      //tableau
+      let table=document.createElement("table")
+      table.className="card__table"
+      card.appendChild(table)
+      let table_header=document.createElement("tr")
+      table.appendChild(table_header)
+      let name_header=document.createElement("th")
+      name_header.textContent="Name"
+      table_header.appendChild(name_header)
+      for(let item of elem.dates){
+        let date=document.createElement("th")
+        date.textContent=item.date
+        table_header.appendChild(date)
+      }
+      for(let item of elem.dates[0].attendees){
+            let ligne=document.createElement("tr")
+            table.appendChild(ligne)
+            let invite=document.createElement("th")
+            invite.textContent=item.name
+            ligne.appendChild(invite)
+            for(let item of elem.dates){
+                let vide=document.createElement("th")
+                vide.textContent=" "
+                ligne.appendChild(vide)                  
+              }
+      }
+      //bouton
+      let add_name=document.createElement("button")
+      add_name.className="add_name"
+      add_name.textContent="Add your name"
+      card.appendChild(add_name)
+      let delete_card=document.createElement("button")
+      delete_card.className="delete_card"
+      delete_card.textContent="Delete"
+      card.appendChild(delete_card)
+
     }
-  });
+  })
