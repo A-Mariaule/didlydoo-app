474c71af0dd0245499e23a31efb68a3dafdae3b2
Benjamin Porsont <porsont.benjamin@gmail.com>
2023-01-05 14:56:25 +0100

    modif

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,36 @@
 //variable
-var main=document.querySelector("main")
-
-
+var main = document.querySelector("main");
 
 fetch("http://localhost:3000/api/events/")
-.then((response)=>(response.json()))
-.then((json)=>{
-    console.log(json)
-    for(let elem of json){
-        //creation de la carte
-        let card=document.createElement("div")
-        card.className="card"
-        main.appendChild(card)
-        //nom de l'event
-        let title=document.createElement("h2")
-        title.className="card__title"
-        card.appendChild(title)
-        title.textContent=elem.name
-        //description
-        let description=document.createElement("p")
-        description.className="card__description"
-        card.appendChild(description)
-        description.textContent=elem.description
-        //liste invité
-        const fetchName= (name)=>fetch("http://localhost:3000/api/attendees/"+name)
-        fetchName(elem.name)
-        .then((response)=>(response.json()))
-        .then((attendees)=>{
-            console.log( attendees)
-        })
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    for (let elem of json) {
+      //creation de la carte
+      let card = document.createElement("div");
+      card.className = "card";
+      main.appendChild(card);
+      //nom de l'event
+      let title = document.createElement("h2");
+      title.className = "card__title";
+      card.appendChild(title);
+      title.textContent = elem.name;
+      //organisateur
+      let organizer = document.createElement("p");
+      organizer.className = "card_organizer";
+      card.appendChild(organizer);
+      organizer.textContent = elem.author;
+      //description
+      let description = document.createElement("p");
+      description.className = "card__description";
+      card.appendChild(description);
+      description.textContent = elem.description;
+      //liste invité
+      const fetchName = (name) => fetch("http://localhost:3000/api/attendees/");
+      fetchName(elem.name)
+        .then((response) => response.json())
+        .then((attendees) => {
+          console.log(attendees);
+        });
     }
-})
-
-
-
+  });
