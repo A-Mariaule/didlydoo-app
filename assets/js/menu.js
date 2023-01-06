191e8d2bbbd9f23f5af056dd8b5a8684e2c1931b
aurorelem <aurorelemaire.pro@gmail.com>
2023-01-06 11:54:09 +0100

    modif

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+let addButton = document.querySelector(".add__button");
+let form = document.querySelector("form");
+
+addButton.addEventListener("click",()=> {
+    form.style.cssText = "display:flex";
+}
+);
